d37f62f387b8186b5b786789315fb1e209eb2d29
sashawasha1337 <sashamarmorstein@gmail.com>
2023-11-20 12:44:22 -0800

    Revert "changing AngularApp to non standalone in order to use calendar stuff"
    This reverts commit 2a1807fca0420bb441aacd6b497aace8e5289562.

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,27 +1,9 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
-=======
-
-@Component({
-  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 >>>>>>> 2a1807fca0420bb441aacd6b497aace8e5289562
   styleUrls: ['./app.component.css']
