89c2a76e5a8f4ec6ccf5784c2dc39ca5008cde58
sashawasha1337 <sashamarmorstein@gmail.com>
2023-11-20 15:20:04 -0800

    new commit with starting ui elements

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
->>>>>>> 2a1807fca0420bb441aacd6b497aace8e5289562
-  styleUrls: ['./app.component.css']
+  
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularApp';
--- AngularApp/src/app/home/home.component.ts
+++ AngularApp/src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
   template: `
     <p>
       home works!
     </p>
-  `,
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
