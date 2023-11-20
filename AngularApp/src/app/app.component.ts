b2af115820a5ea126bc4e8dc808813e1eb039d00
qali03 <81342885+qali03@users.noreply.github.com>
2023-11-20 15:02:23 -0800

    AngularApp

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
->>>>>>> 2a1807fca0420bb441aacd6b497aace8e5289562
+//>>>>>>> 2a1807fca0420bb441aacd6b497aace8e5289562
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
