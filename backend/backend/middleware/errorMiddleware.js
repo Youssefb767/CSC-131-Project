4ba6dbb73b70cb59773d5747df44e28016c0b111
sashawasha1337 <sashamarmorstein@gmail.com>
2023-12-10 14:33:48 -0800

    added meeting controller and routes

---
+++ backend/backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+// Contains the middleware that handles errors
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500
+
+    res.status(statusCode)
+
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+module.exports = {
+    errorHandler
+}
---
+++ backend/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+const app = express()
+
+//Allows the server to accept text being sent back
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on ${port}`))
