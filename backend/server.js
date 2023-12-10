0c89884444fd7b1fda3b6a48851f8d1393f42ab1
sashawasha1337 <147283116+sashawasha1337@users.noreply.github.com>
2023-12-10 15:40:40 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/meeting', require('./routes/meetingRoutes'))
 
 app.use(errorHandler)
 
