9519ff3d103be63569c4202722672031f27a85c7
xmeda <xmeda.zero@gmail.com>
2023-12-10 18:39:58 -0800

    added user availability into the api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,32 @@
 /* Creating routes and controllers let you split up large api file into much smaller,
    more manageable files so that your "server" file isn't a huge wall of text */
 
-const express = require('express')
-const router = express.Router()
-
-//Lets you import the functionalities from the controller file
-const {getAllUser,
-    getSingleUser,
-    createUser,
-    editUser,
-    deleteUser}
-    = require('../controllers/userController')
-
-// You can combine routes that share an endpoint like this:
-router.route('/').get(getAllUser).post(createUser)
-
-router.route('/:id').get(getSingleUser).put(editUser).delete(deleteUser)
-
-//Lets you use the routes in the server file
-module.exports = router
+   const express = require('express')
+   const router = express.Router()
+   
+   //Lets you import the functionalities from the controller file
+   const {getAllUser,
+       getSingleUser,
+       createUser,
+       editUser,
+       deleteUser,
+       getAllUserAvailability,
+       getSingleUserAvailability,
+       createUserAvailability,
+       editUserAvailability,
+       deleteUserAvailability}
+       = require('../controllers/userController')
+   
+   // You can combine routes that share an endpoint like this:
+   router.route('/users').get(getAllUser).post(createUser)
+   
+   router.route('/users/:id').get(getSingleUser).put(editUser).delete(deleteUser)
+   
+   //router.route('/timeslots').get(getAllUserAvailability) timeslots function if we choose to do it this way
+   
+   router.route('/users/:id/availability').get(getSingleUserAvailability).post(createUserAvailability)
+   
+   router.route('/users/:id/availability/:availability_id').put(editUserAvailability).delete(deleteUserAvailability)
+   
+   //Lets you use the routes in the server file
+   module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/users/availability', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
