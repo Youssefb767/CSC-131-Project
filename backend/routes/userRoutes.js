fe6616fc176550b062f385f2529713ea887c9729
Youssef Boujebha <youssefb767@gmail.com>
2023-12-11 20:29:39 -0800

    help me

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,32 @@
 /* Creating routes and controllers let you split up large api file into much smaller,
    more manageable files so that your "server" file isn't a huge wall of text */
-const express = require('express')
-const router = express.Router()
 
-//Lets you import the functionalities from the controller file
-const {getAllUser,
-    getSingleUser,
-    createUser,
-    editUser,
-    deleteUser,
-    getAllUserAvailability,
-    getSingleUserAvailability,
-    createUserAvailability,
-    editUserAvailability,
-    deleteUserAvailability}
-    = require('../controllers/userController')
-
-// You can combine routes that share an endpoint like this:
-router.route('/').get(getAllUser).post(createUser)
-router.route('/:id').get(getSingleUser).put(editUser).delete(deleteUser)
-
-//router.route('/timeslots').get(getAllUserAvailability) timeslots function if we choose to do it this way
-router.route('/availability').get(getSingleUserAvailability).post(createUserAvailability)
-router.route('/:availability_id').put(editUserAvailability).delete(deleteUserAvailability)
-
-//Lets you use the routes in the server file
-module.exports = router
+   const express = require('express')
+   const router = express.Router()
+   
+   //Lets you import the functionalities from the controller file
+   const {getAllUser,
+       getSingleUser,
+       createUser,
+       editUser,
+       deleteUser,
+       getAllUserAvailability,
+       getSingleUserAvailability,
+       createUserAvailability,
+       editUserAvailability,
+       deleteUserAvailability}
+       = require('../controllers/userController')
+   
+   // You can combine routes that share an endpoint like this:
+   router.route('/').get(getAllUser).post(createUser)
+   
+   router.route('/:id').get(getSingleUser).put(editUser).delete(deleteUser)
+   
+   //router.route('/timeslots').get(getAllUserAvailability) timeslots function if we choose to do it this way
+   
+   router.route('/:id/availability').get(getSingleUserAvailability).post(createUserAvailability)
+   
+   router.route('/:id/availability/:availability_id').put(editUserAvailability).delete(deleteUserAvailability)
+   
+   //Lets you use the routes in the server file
+   module.exports = router
