51ea77f9fe690826f0d06361eb57cffe981e4609
sashawasha1337 <sashamarmorstein@gmail.com>
2023-12-12 16:47:42 -0800

    added basic meetings to front end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import React, { useState } from "react";
-import { generateDate, months } from "/Users/qasimali/Desktop/CSC-131-Project/my-app/src/util/calender.js";
+import { generateDate, months } from "/Users/sasha/currentprojecttry/CSC-131-Project/my-app/src/util/calender.js";
 import cn from "./util/cn";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
@@ -9,6 +9,13 @@ export default function Calendar() {
 	const currentDate = dayjs();
 	const [today, setToday] = useState(currentDate);
 	const [selectDate, setSelectDate] = useState(currentDate);
+
+	const meetingData = [
+		{ id: 1, title: "Meeting 1" },
+		{ id: 2, title: "Meeting 2" },
+		// Add more meeting objects as needed
+	];
+
 	return (
 		<div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col">
 			<div className="w-96 h-96 ">
@@ -86,11 +93,20 @@ export default function Calendar() {
 					)}
 				</div>
 			</div>
+
+			{/*displaying meetings*/}
 			<div className="h-96 w-96 sm:px-5">
 				<h1 className=" font-semibold">
 					Schedule for {selectDate.toDate().toDateString()}
 				</h1>
+				<ul>
+					{meetingData.map((meeting) => (
+						<li key={meeting.id}>{meeting.title}</li>
+					))}
+				</ul>
+
 				<p className="text-gray-400">No meetings for today.</p>
+
         <button type="button" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add an Event</button>
         <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2" type="button">Users <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
