0611cce356d6807fd93ef2545d64837001102b4e
qali03 <81342885+qali03@users.noreply.github.com>
2023-12-13 13:14:59 -0800

    Style

--- my-app/src/addEventForm.js
+++ my-app/src/addEventForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {Link} from "react-router-dom";
+import './App.css';
 
 const AddEventForm = ({ onAddEvent }) => {
     const [formData, setFormData] = useState({
@@ -30,20 +31,20 @@ const AddEventForm = ({ onAddEvent }) => {
     };
 
     return (
+        <div class="center">
         <form onSubmit={handleSubmit}>
-            <label>
-                Start Date:
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date:</label>
                 <input
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="date"
                     name="startDate"
                     value={formData.startDate}
                     onChange={handleChange}
                     required
                 />
-            </label>
-            <label>
-                Start Time:
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Time:</label>
                 <input
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="startTime"
                     value={formData.startTime}
@@ -51,10 +52,9 @@ const AddEventForm = ({ onAddEvent }) => {
                     placeholder="HH:MM AM/PM"
                     required
                 />
-            </label>
-            <label>
-                End Time:
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End Time:</label>
                 <input
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="endTime"
                     value={formData.endTime}
@@ -62,21 +62,21 @@ const AddEventForm = ({ onAddEvent }) => {
                     placeholder="HH:MM AM/PM"
                     required
                 />
-            </label>
-            <label>
-                Meeting Description:
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Meeting Description:</label>
                 <textarea
+
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-100 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     name="meetingDescription"
                     value={formData.meetingDescription}
                     onChange={handleChange}
                     required
                 />
-            </label>
-            <button type="submit">Add Event</button>
-            <Link to="/" className="button-style">
-                back to calendar
+            <button type="submit" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add Event</button>
+            <Link to="/" type="button" class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                Back to Calendar
             </Link>
         </form>
+        </div>
     );
 };
 
